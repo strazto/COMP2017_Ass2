8a37243d8f78a1ad5781b7cdb9cd6448d899dd07
matthew_vbox_lubuntu <matthew.strasiotto@gmail.com>
2018-06-02 16:50:12 +1000

    (Ass2)Began implementation of unit tests

--- test.c
+++ test.c
@@ -2,36 +2,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void main (void)
+static post * example_1_posts()
 {
-	query_helper * q_h = malloc(sizeof(query_helper));
-	result * res = NULL;
-	post ** post_res = NULL;
 	size_t n_posts = 20;
 	uint64_t i = 0;
-	int64_t out = 0;
 	post* posts = calloc(n_posts, sizeof(post));
 	post* p = NULL;
+
 	for (i = 0; i < n_posts; i++)
 	{
 		posts[i].pst_id = i;
-		printf("%lu\n", posts[i].pst_id);
+		LOG_V("Setting id of:%lu to post idx: %ld\n", posts[i].pst_id, i);
 	}
-	
-	
-	bst_args_t * args = malloc(sizeof(bst_args_t));
-	args->hi= n_posts;
-	args->lo= 0;
-	
-	args->id = 4;
-	args->arr = (void*) posts;
-	args->arr_type = POST_ARR;
-	
-	out = (int64_t) find_idx((void*) args);
-	printf("%li\n", out);
-	
-	printf("segfault? ln: %lu\n", __LINE__);
-	fflush(stdout);
+
 	p = &posts[2];
 	p->n_reposted = 5;
 	p->reposted_idxs = malloc(sizeof(size_t)*p->n_reposted);
@@ -46,6 +29,16 @@ void main (void)
 	p->reposted_idxs[0] = 15;
 	p->reposted_idxs[1] = 17;
 	p->reposted_idxs[2] = 13;
+	return posts;
+}
+
+
+void main (void)
+{
+	query_helper * q_h = malloc(sizeof(query_helper));
+	result * res = NULL;
+	post ** post_res = NULL;
+	
 	
 	
 	res = find_all_reposts(posts, n_posts, 2, q_h);
--- test.h
+++ test.h
@@ -1,5 +1,14 @@
+#ifndef TEST_H
+#define TEST_H
 #include "supergraph.h"
 #include "mine.h"
+#include <stddef.h>
+#include <setjmp.j>
+#include <cmocka.h>
+#include <stdio.h>
+#include "log.h"
+
+static post * example_1_posts();
 
 
-
+#endif
