3934b1a63117cc1ac1ab9136d39c549846d32faa
matthew_vbox_lubuntu <matthew.strasiotto@gmail.com>
2018-06-05 04:27:05 +1000

    Refactoring into functions

--- python/edit.py
+++ python/edit.py
@@ -9,20 +9,27 @@ def toggle_df(frame, row, column):
     print("Row:" + str(row) + "Col" + str(column))
     print(frame)
 
+def bool_df(root, df):
+	for c in range(len(df.columns)):
+	    tk.Label(root, text=str(df.columns[c])).grid(row=0,column=c+1)
+
+	for r in df.index:
+	    tk.Label(root, text=str(df.index[r])).grid(row=r+1,column=0)  
+	    for c in range(len(df.columns)):
+	        button = tk.Checkbutton(root, borderwidth=1, var=tk.IntVar(), command=partial(toggle_df,df,r,c))
+	        if (df.iloc[r,c]):
+	            button.select()
+	        button.grid(row=r + 1,column=c + 1)
+
+
 df = pd.DataFrame(data={1 : [0, 1], 2 : [0, 0], 3 : [1, 1]}, dtype=np.int8)
 
 print(df)
 
 root = tk.Tk(  )
 
-for c in range(len(df.columns)):
-    tk.Label(root, text=str(df.columns[c])).grid(row=0,column=c+1)
+bool_df(root, df)
 
-for r in df.index:
-    tk.Label(root, text=str(df.index[r])).grid(row=r+1,column=0)  
-    for c in range(len(df.columns)):
-        button = tk.Checkbutton(root, borderwidth=1, var=tk.IntVar(), command=partial(toggle_df,df,r,c))
-        if (df.iloc[r,c]):
-            button.select()
-        button.grid(row=r + 1,column=c + 1)
 root.mainloop(  )
+
+
