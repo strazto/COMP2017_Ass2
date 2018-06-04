d0fd21e321a6bcbc3607f9e8f1306a3e8233dd8e
matthew_vbox_lubuntu <matthew.strasiotto@gmail.com>
2018-06-04 16:51:55 +1000

    Race Cond Observed, now can try and make threadsafe

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include "DLL_unsafe.h"
 #include "thread_test.h"
-
+#include "log.h"
 
 void main(void)
 {
@@ -10,8 +10,8 @@ void main(void)
 	int64_t n_removed = 0;
 	void * retval;
 	pthread_t * threads = malloc(sizeof(pthread_t)*n_threads);
-	
-	
+	int to_push = 0;
+	int64_t count = 0;	
 
 	if (!list)
 	{
@@ -22,9 +22,12 @@ void main(void)
 		enqueue(list, (void*) i);
 	}
 
-	for (int i = 30; i < 50; i++)
+	for (int i = 0; i < 50; i++)
 	{
-		push(list, (void*) ( i  % n_elements));
+		to_push = i;
+		if (! (i %3)) to_push = 7;
+
+		push(list, (void*) ( to_push));
 	}
 
 
@@ -34,24 +37,36 @@ void main(void)
 
 	while (current && current != list->tail)
 	{
-		printf("Element: %i\n", (int) current->val);
+		printf("Element: %li\n", (int64_t) current->val);
+		if (7 == (int64_t) current->val) count++;
 		current = current->prev;
 	}
-	
+	printf("N 7s %li", count);
+	count = 0;
 	int64_dll_args_t args = 
 	{
 		.list = list,
 		.element = 7
 	};
 
-	for (int i; i < n_threads; i++)
+	for (int i = 0; i < n_threads; i++)
 	{
+		LOG_I("Initializing thread %i", i);
 		pthread_create(&threads[i], NULL, remove_number, &args);
 	}
 
-	for (int i; i < n_threads; i++)
+	for (int i = 0; i < n_threads; i++)
 	{
 		pthread_join(threads[i], &retval);
-		printf("Thread %i, n removed %li", (int) retval);
+		LOG_V("Thread %i, n removed %li", i,(int64_t) retval);
+		count += (int64_t) retval;
+
+	}
+	printf("N removed total: %li", count);
+
+	printf("List size: %lu\n",list->size);
+	for (current = first(list); current && current != list->tail; current = current->prev)
+	{
+		printf("Element: %li\n", (int64_t) current->val);		
 	}
 }
