b862c435a0ed992701c9ccf5675bdeb7a617170e
matthew_vbox_lubuntu <matthew.strasiotto@gmail.com>
2018-06-09 13:43:22 +1000

    Refactoring done to allow shared headers

--- test.h
+++ test.h
@@ -1,6 +1,5 @@
 #ifndef TEST_H
 #define TEST_H
-#include "supergraph.h"
 #include <stdarg.h>
 #include <stddef.h>
 #include <setjmp.h>
@@ -8,7 +7,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include "log.h"
+#include "../supergraph.h"
+#include "../log.h"
 #include "properties.h"
 
 #endif
